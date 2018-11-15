f96cbbb4947ae6d234b10b2159f56484a1607d1b
Yury Lavrukhin <yurka7321@gmail.com>
2018-11-16 01:47:20 +0300

    Remove unnecessary files

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,36 @@
-import * as React from 'react';
+import React, { Component } from 'react';
+import { cn } from '@bem-react/classname';
 
-export const Card = ({ text, className }) => (
-  <div className="stiring">tefrfeoikoij</div>
-);
+// Libraries
+const classNames = require('classnames');
+
+const card = cn('Card');
+
+export const Card = (w, props) => {
+  const headerClass = classNames({
+    [card('HeaderWrap')]: true,
+    'critical': props.type === 'critical',
+    'have-description': props.description
+  });
+  return (
+    <div className={card({ small: true })}>
+      <div className={headerClass}>
+        <div className={card('Header', { small: true })}>
+          <img className={card('Logo', { small: true })} src={`./assets/${props.icon}.svg`} alt="" />
+          <p className={card('Title', { small: true })}>{props.title}</p>
+        </div>
+        <div className={card('Status', { small: true })}>
+          <p className={card('Source', { small: true }) + ' status-text'}>{props.source}</p>
+          <p className={card('Time', { small: true }) + ' status-text'}>{props.time}</p>
+        </div>
+      </div>
+      {
+        props.description &&
+        <div className={card('Description', { small: true })}>
+          <p className={card('DescriptionParagraph', { small: true })}>{props.description}</p>
+        </div>
+      }
+    </div>
+  );
+}
+
--- src/components/Card/_size/Card_large.jsx
+++ src/components/Card/_size/Card_large.jsx
@@ -8,50 +8,50 @@ const classNames = require('classnames');
 const card = cn('Card');
 
 const Card_large = (w, props) => {
-    const headerClass = classNames({
-      [card('HeaderWrap', { large: true })]: true,
-      'critical': props.type === 'critical',
-      'have-description': props.description
-    });
-    return (
-      <div className={card({ large: true })}>
-        <div className={headerClass}>
-          <div className={card('Header', { large: true })}>
-            <img className={card('Logo', { large: true })} src={`./assets/${props.icon}.svg`} alt="" />
-            <p className={card('Title', { large: true })}>{props.title}</p>
-          </div>
-          <div className={card('Status', { large: true })}>
-            <p className={card('Source', { large: true })}>{props.source}</p>
-            <p className={card('Time', { large: true })}>{props.time}</p>
-          </div>
+  const headerClass = classNames({
+    [card('HeaderWrap', { large: true })]: true,
+    'critical': props.type === 'critical',
+    'have-description': props.description
+  });
+  return (
+    <div className={card({ large: true })}>
+      <div className={headerClass}>
+        <div className={card('Header', { large: true })}>
+          <img className={card('Logo', { large: true })} src={`./assets/${props.icon}.svg`} alt="" />
+          <p className={card('Title', { large: true })}>{props.title}</p>
         </div>
-        {
-          props.description &&
-          <div className={card('Description', { large: true })}>
-            <p className={card('DescriptionParagraph', { large: true })}>{props.description}</p>
-            {
-              props.data.type === 'graph' &&
-              <div className={card('ImageContainer')}>
-                <img src="./assets/richdata.svg" className={card('Image')} alt="" />
-              </div>
-            }
-            {
-            props.data.image &&
-            <div className={card('ImageContainer')} id="hoover-container">
-            <img className={card('Image')}
-              id="hoover"
-              touch-action="none"
-              srcSet="./assets/bitmap.png 768w,
-              ./assets/bitmap2x.png 1366w,
-              ./assets/bitmap3x.png 1920w"
-              src="./assets/bitmap2x.png"
-              alt=""/>
+        <div className={card('Status', { large: true })}>
+          <p className={card('Source', { large: true })}>{props.source}</p>
+          <p className={card('Time', { large: true })}>{props.time}</p>
+        </div>
+      </div>
+      {
+        props.description &&
+        <div className={card('Description', { large: true })}>
+          <p className={card('DescriptionParagraph', { large: true })}>{props.description}</p>
+          {
+            props.data.type === 'graph' &&
+            <div className={card('ImageContainer')}>
+              <img src="./assets/richdata.svg" className={card('Image')} alt="" />
             </div>
           }
+          {
+          props.data.image &&
+          <div className={card('ImageContainer')} id="hoover-container">
+          <img className={card('Image')}
+            id="hoover"
+            touch-action="none"
+            srcSet="./assets/bitmap.png 768w,
+            ./assets/bitmap2x.png 1366w,
+            ./assets/bitmap3x.png 1920w"
+            src="./assets/bitmap2x.png"
+            alt=""/>
           </div>
         }
-      </div>
-    );
+        </div>
+      }
+    </div>
+  );
 }
 
 export const cardLarge = withBemMod(
