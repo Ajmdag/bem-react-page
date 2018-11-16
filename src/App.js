f85a420a4b317241eb0a0d74cb4c43806ace3c87
Yury Lavrukhin <yurka7321@gmail.com>
2018-11-16 16:22:56 +0300

    Remove commented code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 
-// Components
-// import Card_small from './components/Card/_size/Card_small';
-// import Card_medium from './components/Card/_size/Card_medium';
-// import Card_large from './components/Card/_size/Card_large';
-
 import { Card } from './components/Card/index';
 
 // Data
